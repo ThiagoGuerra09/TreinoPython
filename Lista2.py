cce313b3edb89d4778c901917ab58b7592d3e394
Thiago Guerra <82849005+ThiagoGuerra09@users.noreply.github.com>
2022-07-28 15:12:38 -0300

    Add files via upload

---
+++ Lista2.py
@@ -0,0 +1,39 @@
+#1
+# numero1=input("Digite um numero:\n")
+# numero2=input("Digite um numero:\n")
+# result=int(numero1)-int(numero2) #positivo n1 é maior, negativo n2 é maior
+# if(result!=0):
+#     if(result>0):
+#         print(numero1)
+#     else:
+#         print(numero2)
+# else:
+#     print("São iguais")
+
+#2
+# numero1=input("Digite um numero:\n")
+# if(int(numero1)>=0):
+#     print("Número positivo")
+# else:
+#     print("Número negativo")
+
+#3
+# sexo=input("Digite seu sexo F ou M:\n")
+# if(sexo=="F"):
+#     print("Feminino")
+# else:
+#     if(sexo=="M"):
+#         print("Masculino")
+#     else:
+#         print("Sexo inválido")
+
+#4
+# def vogal(letra):
+#     if(letra=="a"or letra=="A" or letra=="b" or letra=="B"or letra=="c"or letra=="C"or letra=="d"or letra=="D"or letra=="e"or letra=="E"):
+#         print("vogal")
+#     else:
+#         print("consoante")
+# entrada=input("Digite uma letra\n")
+# vogal(entrada)
+
+#5
