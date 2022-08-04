7ddc8d952d131d52d30327bbce69ea24db297755
Thiago Guerra <82849005+ThiagoGuerra09@users.noreply.github.com>
2022-08-04 18:01:08 -0300

    Update EstruturaDeDecisao.py

--- EstruturaDeDecisao.py
+++ EstruturaDeDecisao.py
@@ -175,3 +175,23 @@ if(bruto>2500):
 
 print("Total de descontos:\n", bruto-liquido)
 print("Salario liquido:\n", liquido) 
+
+13
+dia=input("1-Domingo, 2-Segunda, 3-Terça, 4-Quarta, 5-Quinta, 6-Sexta, 7-Sábado\n")
+dia=int(dia)
+if(dia==1):
+    print("Domingo")
+if(dia==2):
+    print("Segunda")
+if(dia==3):
+    print("Terça")
+if(dia==4):
+    print("Quarta")
+if(dia==5):
+    print("Quinta")
+if(dia==6):
+    print("Sexta")
+if(dia==7):
+    print("Sábado")
+if(dia==0 or dia>7):
+    print("Valor inválido")
